504f6a33245a24e7a698e73fd0acfc5344f3b29d
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-06-29 14:46:42 +0500

    add slash

--- restapi/urls.py
+++ restapi/urls.py
@@ -5,16 +5,16 @@ from .views import *
 
 urlpatterns = [
   # API
-  path('api/category', CategoryCreateList.as_view()),
+  path('api/category/', CategoryCreateList.as_view()),
   path('api/category/<str:title>', CategoryDetail.as_view()),
-  path('api/item', ItemCreateList.as_view()),
+  path('api/item/', ItemCreateList.as_view()),
   path('api/item/<str:slug>', ItemDetail.as_view()),
-  path('api/review', ReviewCreateList.as_view()),
+  path('api/review/', ReviewCreateList.as_view()),
   path('api/review/<str:slug>', ReviewDetail.as_view()),
   # render
   path('', CategoryList.as_view()),
   path('category/<str:title>', CategoryList.as_view()),
-  path('categories', CategoryList.as_view()),
+  path('categories/', CategoryList.as_view()),
   path('item/<str:slug>', CategoryList.as_view()),
-  path('about', CategoryList.as_view())
+  path('about/', CategoryList.as_view())
 ]
