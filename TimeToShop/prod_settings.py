50e25eddf424e5b68609d8c20434c632ef2dbd1f
Kostya Nikiforov <kostya.nik.3854@gmail.com>
2019-06-29 14:41:47 +0500

    add slash to urls

--- TimeToShop/prod_settings.py
+++ TimeToShop/prod_settings.py
@@ -6,11 +6,14 @@ SECRET_KEY = 'k@w1h-n8r*i!67sj)+osn=t0m^b8vqsd5s#kf1f6c2wtt44m(z'
 
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['timetoshop.pythonanywhere.com']
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'test.sqlite3'),
     }
 }
+STATIC_ROOT = '/home/TimeToShop/TimeToShop/static/'
